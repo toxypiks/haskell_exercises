456b0f968165a399f5a1ac1a5fe2b7669e0e3bcc
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-29 10:45:50 +0200

    Practice list comprehension

--- main.hs
+++ main.hs
@@ -9,3 +9,9 @@ fibb :: Int -> Int
 fibb n | n == 0 = 1
        | n == 1 = 1
        | otherwise = (fibb (n - 1)) + (fibb (n - 2))
+
+doubleSmallNumber :: Int -> Int
+doubleSmallNumber x = if x > 100 then x else x*2
+
+boombang :: [Int] -> [String]
+boombang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
