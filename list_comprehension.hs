526a398cb9adf2f7f6786a4c8bc79540df8205bd
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-31 16:52:48 +0200

    Add more exercises for list comprehension

---
+++ list_comprehension.hs
@@ -0,0 +1,26 @@
+{- List comprehension in haskell follows the mathematical definition of such.
+   Firstly, the output function is defined. After a pipe, the input set is speci   fied, followed by a predicate.-}
+
+-- simple example that works with x values from 1 to 10 but only the ones greater than  12
+
+x = [x*2 | x <- [1..10], x*2 >= 12]
+
+-- function returns BOOM! if x < 10 and odd, else BANG! and all x have to be odd for both conditions
+boombang :: [Int] -> [String]
+boombang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- its possible to include several predicates
+
+x1 = [x | x <- [10..20], x /= 13, x /=15, x /= 19]
+
+-- and several lists
+
+x2 = [x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50]
+
+-- length function
+
+my_lenght xs = sum [1 | _ <- xs]
+
+-- removing all lowercase letters from a string
+
+removeNonUppercase str = [c | c <- str, c `elem` ['A'..'Z']]
--- main.hs
+++ main.hs
@@ -13,5 +13,25 @@ fibb n | n == 0 = 1
 doubleSmallNumber :: Int -> Int
 doubleSmallNumber x = if x > 100 then x else x*2
 
-boombang :: [Int] -> [String]
-boombang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+my_max :: Int -> Int -> Int
+my_max a b | a > b = a
+           |otherwise = b
+
+my_if b a1 a2 = case b of
+                  True -> a1
+                  False -> a2
+
+-- syntactic sugar for case syntax
+my_if' True a1 _ = a1
+my_if' False _ a2 = a2
+
+squared_diff x y = (x-y) * (x-y)
+
+squared_diff1 x y =
+    let d = x - y
+    in d * d
+
+squared_diff2 x y = d * d
+    where d = x - y
+
+-- where and let are basically the same but you'll find where more often in code from other ppl
