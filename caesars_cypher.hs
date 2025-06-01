dcdc34afd731ce4337be95a929551fd68a904fb5
toxypiks <HannaHinkelbein89@gmx.de>
2025-06-01 12:53:19 +0200

    Define Alphabet data type and use it to define Alphabets in caesers cyper

--- caesars_cypher.hs
+++ caesars_cypher.hs
@@ -1,6 +1,35 @@
+type Alphabet = [Char]
+
+lowerAlphabet :: Alphabet
+lowerAlphabet = ['a' .. 'z']
+
+upperAlphabet :: Alphabet
+upperAlphabet = ['A' .. 'Z']
+
+digits :: Alphabet
+digits = ['0' .. '9']
+
 -- find index of char in list of chars
 
-indexOf :: Char -> [Char] -> Int
+indexOf :: Char -> Alphabet -> Int
 indexOf ch [] = undefined
 indexOf ch (x:xs) | x == ch = 0
                   | otherwise = 1 + indexOf ch xs
+
+-- retrieve an element at a certain index
+
+{-elemAt :: Int -> [Char] -> Char
+elemAt x [] = undefined
+elemAt x (y:ys) | x == indexOf y ys = y
+elemAt x (y:ys) | otherwise = elemAt x+1 ys
+-}
+
+-- list of alphabet !! index
+-- index = index of char in upperAlphabet + offset
+-- index `mod` 26 to circle values between 0 and 26
+
+upperRot :: Int -> Char -> Char
+upperRot n ch = upperAlphabet !! ((indexOf ch upperAlphabet + n) `mod` 26)
+
+lowerRot :: Int -> Char -> Char
+lowerRot n ch = lowerAlphabet !! ((indexOf ch lowerAlphabet + n) `mod` 26)
