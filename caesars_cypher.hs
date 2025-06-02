9ab81f2232771993d5fed5947fc5b577bfbfffa1
toxypiks <HannaHinkelbein89@gmx.de>
2025-06-02 11:04:28 +0200

    Add functions to test if char is element of defined lists

--- caesars_cypher.hs
+++ caesars_cypher.hs
@@ -1,23 +1,33 @@
 type Alphabet = [Char]
 
 lowerAlphabet :: Alphabet
-lowerAlphabet = ['a' .. 'z']
+lowerAlphabet = ['a'..'z']
 
 upperAlphabet :: Alphabet
-upperAlphabet = ['A' .. 'Z']
+upperAlphabet = ['A'..'Z']
 
 digits :: Alphabet
-digits = ['0' .. '9']
+digits = ['0'..'9']
+
+isLower :: Char -> Bool
+isLower char = char `elem` lowerAlphabet
+
+isUpper :: Char -> Bool
+isUpper char = char `elem` upperAlphabet
+
+isDigit :: Char -> Bool
+isDigit char = char `elem` digits
+
+isMisc :: Char -> Bool
+isMisc char = not (isUpper char || isLower char || isDigit char)
 
 -- find index of char in list of chars
-
 indexOf :: Char -> Alphabet -> Int
 indexOf ch [] = undefined
 indexOf ch (x:xs) | x == ch = 0
                   | otherwise = 1 + indexOf ch xs
 
 -- retrieve an element at a certain index
-
 elemAt :: [a] -> Int -> a
 elemAt [] _ = undefined
 elemAt (x:xs) n
@@ -25,14 +35,13 @@ elemAt (x:xs) n
        | n == 0 = x
        | otherwise = elemAt xs (n - 1)
 
-
 -- list of alphabet !! index
 -- index = index of char in upperAlphabet + offset
 -- index `mod` 26 to circle values between 0 and 26
 
 alphabetRot :: Alphabet -> Int -> Char -> Char
 alphabetRot alphabet n ch =
-    alphabet !! ((indexOf ch alphabet + n) `mod` length alphabet)
+    alphabet !! (((indexOf ch alphabet) + n) `mod` length alphabet)
 
 upperRot :: Int -> Char -> Char
 upperRot n ch = alphabetRot upperAlphabet n ch
