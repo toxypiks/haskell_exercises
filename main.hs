85d2a04614f7aa7e7be807c13a864bb761a0d17e
toxypiks <HannaHinkelbein89@gmx.de>
2025-06-20 19:12:23 +0200

    Add exercises

--- main.hs
+++ main.hs
@@ -35,3 +35,85 @@ squared_diff2 x y = d * d
     where d = x - y
 
 -- where and let are basically the same but you'll find where more often in code from other ppl
+
+fst (x, y) = x
+snd (x, y) = y
+
+first (x, y, z) = x
+second (x, y, z) = y
+third (x, y, z) = z
+
+-- you can also use the dont care operator e.g. third (_, _, z) = z
+
+head' (x:xs) = x
+tail' (x:xs) = xs
+
+null' [] = True
+null' (_:_) = False
+
+len [] = 0
+len (x:xs) = 1 + len(xs)
+
+last' [] = error "init: empty list"
+last' [x] = x
+last' (_: xs) = last' xs
+
+init' [] = error "init: empty list"
+init' [x] = [] -- alternative: [_] = []
+init' (x:xs) = x:init' xs
+
+[] +++ ys = ys
+(x:xs) +++ ys = x:(xs +++ ys)
+
+my_reverse [] = []
+my_reverse (x:xs) =  (my_reverse xs) ++ [x] -- without parantheses [x] no ++ list operator
+
+my_reverse' xs = f xs []
+     where f [] acc = acc
+           f (y:ys) acc = f ys (y:acc) -- Umwandlung in endständige Rekursion
+
+my_elem _ [] = False
+my_elem elem (x:xs)
+  | elem == x = True
+  | otherwise = my_elem elem xs
+
+
+teiler n = [x | x <- [1..n], n `mod` x == 0]
+
+mymap fct [] = []
+mymap fct (x:xs) = fct x:mymap fct xs
+
+mymaplc f xs = [ f x | x <- xs]
+
+cons_if f x xs = if f x then x:xs else xs
+
+my_filter arg [] = []
+my_filter arg (x:xs) = cons_if arg x (my_filter arg xs)
+
+my_filterlc f xs = [x | x <- xs, f x]
+
+my_sum [] = 0
+my_sum (x:xs) = x + my_sum xs
+
+my_sumf xs = foldl helper 0 xs -- start value 0
+             where helper x y = x + y
+
+my_zip _ [] _ = []
+my_zip _ _ [] = []
+my_zip fct (x:xs) (y:ys) = fct x y : my_zip fct xs ys
+
+stringList' [] = []
+stringList' (x:xs) = show x:stringList' xs
+
+data Konto = Konto {
+      nummer :: Int,
+      inhaber :: String,
+      stand :: Float} deriving (Eq, Show)
+
+{--main = do
+  putStrLn "Type temperature in Celsius: "
+  tmp_celsius <- getLine
+  let tmp_fahrenheit = read tmp_celsius * 9.0 / 5.0 + 32.0
+  let response = "Temperature in Fahrenheit: " ++ show tmp_fahrenheit
+  putStrLn response
+--}
